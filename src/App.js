f3504df0176205cf23584e9cae00d8c9a942f6c5
Mon MacBook <justine.fernandez1990@gmail.com>
2023-03-02 10:20:08 +0100

    add Component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import react from "react";
+import RoundOne from "./components/roundedDesign/RoundOne";
 
 function App() {
   return (
     <div className="w-full h-screen font-bodyfont text-textColor bg-black relative">
       <div className="w-full h-full absolute top-0 left-0 z-10">
+        <RoundOne />
       </div>
     </div>
   );
